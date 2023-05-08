6c85e84584e462537807a9baf7c7e1f807c1de5d
Carlos May <carlosmg.may@icloud.com>
2023-05-08 14:18:35 -0400

    [CREATE] Activity 3

---
+++ 00-Activities/ActivityThree.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace _00_Activities
+{
+	[Activity (Label = "ActivityThree")]			
+	public class ActivityThree : Activity
+	{
+        EditText _editText = null;
+
+        protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+            //->Layout to render
+            SetContentView(Resource.Layout.activity_three);
+
+            //result
+            var btnGoHomeUI = FindViewById<Button>( Resource.Id.btn_goHome_ui );
+            _editText = FindViewById<EditText>(Resource.Id.txt_response_ui);
+
+            btnGoHomeUI.Click += GoHomeHandler;
+        }
+
+        private void GoHomeHandler(object sender, EventArgs e)
+        {
+            var message = _editText.Text + " Activity 3";
+            var intent = new Intent();
+            intent.PutExtra("message", message);
+            SetResult(Result.Ok, intent);
+            Finish();
+        }
+    }
+}
+
