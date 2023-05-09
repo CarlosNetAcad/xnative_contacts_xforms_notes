bc4fa98c39262dd1820a06fae58fb971b17a8431
Carlos May <carlosmg.may@icloud.com>
2023-05-09 14:05:52 -0400

    Create Contact.cs
    Contact Entity

---
+++ 00-Activities/src/Contacts/Domain/Entity/Contact.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace _00_Activities.src.Contacts.Domain.Entity
+{
+	public class Contact
+	{
+        public string ID { get; set; } = Guid.NewGuid().ToString();
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public DateTime CreatedAt { get; set; }
+	}
+}
+
