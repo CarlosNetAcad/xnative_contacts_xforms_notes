d8e5bc3a94c62da6e42ac21c32cdd89b7678d9b9
Carlos May <carlosmg.may@icloud.com>
2023-05-09 18:46:57 -0400

    Update ContactDetailActivity.cs
    *CATCHING extras to show in Activity.

--- 00-Activities/src/Contacts/App/Presenter/ContactDetailActivity.cs
+++ 00-Activities/src/Contacts/App/Presenter/ContactDetailActivity.cs
@@ -10,17 +10,34 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Newtonsoft.Json;
+using _00_Activities.src.Contacts.Domain.Entity;
 
 namespace _00_Activities
 {
-	[Activity (Label = "ContactDetailActivity")]			
+	[Activity (
+		Label = "ContactDetailActivity"
+	)]			
 	public class ContactDetailActivity : Activity
 	{
+		private EditText txtContactFullNameUI,
+						txtContactPhoneUI;
+
 		protected override void OnCreate (Bundle savedInstanceState)
 		{
 			base.OnCreate (savedInstanceState);
 
-			// Create your application here
+			SetContentView( Resource.Layout.contact_detail_layout );
+
+			txtContactFullNameUI  = FindViewById<EditText>( Resource.Id.txt_contactFullName_ui );
+			txtContactPhoneUI     = FindViewById<EditText>( Resource.Id.txt_contactPhone_ui );
+
+			string contactJSON = Intent.GetStringExtra( "contact_json" );
+			Console.WriteLine( contactJSON );
+			//var contact        =JsonConvert.DeserializeObject<Contact>( contactJSON );
+
+			//txtContactFullNameUI.Text = contact?.FullName.ToString();
+			//txtContactPhoneUI.Text    = contact?.Phone.ToString();
 		}
 	}
 }
