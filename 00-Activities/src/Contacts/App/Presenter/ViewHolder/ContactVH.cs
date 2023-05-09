b69be641655b6b93188d0df1681fbe83a15ddbe6
Carlos May <carlosmg.may@icloud.com>
2023-05-09 18:48:08 -0400

    Update ContactVH.cs
    *ADD -OnClickHandler to sent the clicked position.

--- 00-Activities/src/Contacts/App/Presenter/ViewHolder/ContactVH.cs
+++ 00-Activities/src/Contacts/App/Presenter/ViewHolder/ContactVH.cs
@@ -8,13 +8,27 @@ namespace _00_Activities.src.Contacts.App.Presenter.ViewHolders
 	public class ContactVH : RecyclerView.ViewHolder
 	{
 		public TextView Name { get; private set; }
+
 		public TextView Phone { get; private set; }
 
-		public ContactVH( View itemView) : base( itemView ) 
+		private Action<int> _action;
+
+		public ContactVH( View itemView, Action<int> action) : base( itemView ) 
 		{
 			Name  = itemView.FindViewById<TextView>( Resource.Id.lbl_contact_name);
 			Phone = itemView.FindViewById<TextView>( Resource.Id.lbl_contact_phone );
+
+			_action = action;
+
+			itemView.Click += OnClickHandler;
 		}
+
+        private void OnClickHandler( object sender, EventArgs e)
+		{
+#pragma warning disable CS0618 // Type or member is obsolete
+            _action?.Invoke( this.Position );
+#pragma warning restore CS0618 // Type or member is obsolete
+        }
 	}
 }
 
