055c9a110dd7c21746a766bc19c0ac8337d47bf8
Carlos May <carlosmg.may@icloud.com>
2023-05-09 14:05:32 -0400

    [CREATE] Contact View Holder

---
+++ 00-Activities/src/Contacts/App/Presenter/ViewHolder/ContactVH.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Android.Views;
+using Android.Widget;
+using AndroidX.RecyclerView.Widget;
+
+namespace _00_Activities.src.Contacts.App.Presenter.ViewHolders
+{
+	public class ContactVH : RecyclerView.ViewHolder
+	{
+		public TextView Name { get; private set; }
+		public TextView Phone { get; private set; }
+
+		public ContactVH( View itemView) : base( itemView ) 
+		{
+			Name  = itemView.FindViewById<TextView>( Resource.Id.lbl_contact_name);
+			Phone = itemView.FindViewById<TextView>( Resource.Id.lbl_contact_phone );
+		}
+	}
+}
+
