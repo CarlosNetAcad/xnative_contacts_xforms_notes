066a76aaa048351b0ce5e1f499a024fb8c82b778
Carlos May <carlosmg.may@icloud.com>
2023-05-09 14:06:00 -0400

    Create ContactAdapter.cs

---
+++ 00-Activities/src/Contacts/Domain/Service/ContactAdapter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.Views;
+using AndroidX.RecyclerView.Widget;
+using _00_Activities.src.Contacts.Domain.Entity;
+using _00_Activities.src.Contacts.App.Presenter.ViewHolders;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace _00_Activities.src.Contacts.Domain.Service
+{
+	public class ContactAdapter : RecyclerView.Adapter
+	{
+		private IList<Contact> _contactList = new List<Contact>();
+
+		public ContactAdapter( IList<Contact> contacts)
+		{
+			_contactList = contacts;
+		}
+
+        public override int ItemCount => _contactList.Count;
+
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+			var currentContact    = _contactList[position];
+			var contactViewHolder = holder as ContactVH;
+
+			contactViewHolder.Name.Text  = currentContact.FullName;
+			contactViewHolder.Phone.Text = currentContact.Phone;
+        }
+
+		public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType )
+		{
+			View itemView = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.ContactListComponent, parent, false );
+
+			var holder    = new ContactVH( itemView );
+
+			return holder;
+		}
+
+    }
+}
+
