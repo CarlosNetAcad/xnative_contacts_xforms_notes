248eeb578d85a0bc2b23ae88785d7290fe080769
Carlos May <carlosmg.may@icloud.com>
2023-05-10 16:18:56 -0400

    Update Contact.cs

--- 00-Activities/src/Contacts/Domain/Entity/Contact.cs
+++ 00-Activities/src/Contacts/Domain/Entity/Contact.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using SQLite;
+
 namespace _00_Activities.src.Contacts.Domain.Entity
 {
 	public class Contact
 	{
+        [PrimaryKey]
         public string ID { get; set; } = Guid.NewGuid().ToString();
         public string FullName { get; set; }
+        [Unique]
         public string Phone { get; set; }
         public DateTime CreatedAt { get; set; }
 	}
