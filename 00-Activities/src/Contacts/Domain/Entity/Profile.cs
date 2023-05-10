791bac72799f3f499d42f9916e17f1e09e66ec0d
Carlos May <carlosmg.may@icloud.com>
2023-05-10 16:18:58 -0400

    Create Profile.cs

---
+++ 00-Activities/src/Contacts/Domain/Entity/Profile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SQLite;
+
+namespace _00_Activities.src.Contacts.Domain.Entity
+{
+	public class Profile
+	{
+        [PrimaryKey]
+        public string ID { get; set; } = Guid.NewGuid().ToString();
+        [Unique]
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public DateTime CreatedAt { get; set; }
+	}
+}
+
