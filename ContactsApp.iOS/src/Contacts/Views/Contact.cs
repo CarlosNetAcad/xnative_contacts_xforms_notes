3f71905a33bbf75136d2f9b6bb0a8f5ff6239643
Carlos May <carlosmg.may@icloud.com>
2023-05-11 11:58:47 -0400

    [CREATE] Two controllers
    *CREATE ContactVH  View Controller.
    *CREATE ThirdOneVH View Controller.

---
+++ ContactsApp.iOS/src/Contacts/Views/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using UIKit;
+
+namespace ContactsApp.iOS.src.Contacts.Views
+{
+	public partial class Contact : UIViewController
+	{
+		public Contact () : base ("Contact", null)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
+
---
+++ ContactsApp.iOS/src/Contacts/Views/ThirdOne.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using UIKit;
+
+namespace ContactsApp.iOS.src.Contacts.Views
+{
+	public partial class ThirdOne : UIViewController
+	{
+		public ThirdOne () : base ("ThirdOne", null)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
+
