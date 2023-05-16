dd0e9ded180b42103cda5e32d875d887f8111daa
Carlos May <carlosmg.may@icloud.com>
2023-05-16 12:35:57 -0400

    [CREATE] New App icon

---
+++ 00-Activities/src/Contacts/App/Presenter/SplashScreen.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace _00_Activities.src.Contacts.App.Presenter
+{
+	[Activity (
+		Label = "SplashScreen",
+		NoHistory = true,
+		MainLauncher = false,
+		Theme = "@style/AppTheme.NoActionBar"
+    )]			
+	public class SplashScreen : Activity
+	{
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			// Create your application here
+		}
+	}
+}
+
