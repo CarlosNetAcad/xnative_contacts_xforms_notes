6908bb74111ea35252b5d5d234ea8a741383021f
Carlos May <carlosmg.may@icloud.com>
2023-05-16 13:00:54 -0400

    [UPDATE] ADD splash activity

--- 00-Activities/src/Contacts/App/Presenter/SplashScreen.cs
+++ 00-Activities/src/Contacts/App/Presenter/SplashScreen.cs
@@ -3,30 +3,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using AndroidX.AppCompat.App;
 
 namespace _00_Activities.src.Contacts.App.Presenter
 {
 	[Activity (
 		Label = "SplashScreen",
 		NoHistory = true,
-		MainLauncher = false,
+		MainLauncher = true,
 		Theme = "@style/AppTheme.NoActionBar"
     )]			
-	public class SplashScreen : Activity
+	public class SplashScreen : AppCompatActivity
 	{
 		protected override void OnCreate (Bundle savedInstanceState)
 		{
 			base.OnCreate (savedInstanceState);
 
-			// Create your application here
+			SetContentView( Resource.Layout.splash_loucher );
 		}
-	}
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+			Task startupWork = new Task( () => { SimulateStartup(); } );
+
+			startupWork.Start();
+        }
+
+        async void SimulateStartup()
+        {
+			await Task.Delay( 3000 );
+			StartActivity( new Intent( Application.Context, typeof( ContactsActivity ) ) ); 
+        }
+    }
 }
 
