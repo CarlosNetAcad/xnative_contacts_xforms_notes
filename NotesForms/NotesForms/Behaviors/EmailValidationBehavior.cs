7ca2a74c231bed193f81904e275fc5e013e73dae
Carlos May <carlosmg.may@icloud.com>
2023-05-25 18:41:26 -0400

    [UPDATE] ADD Behavior

---
+++ NotesForms/NotesForms/Behaviors/EmailValidationBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace NotesForms.Behaviors
+{
+	public class EmailValidationBehavior:Behavior<Entry>
+	{
+        #region const
+        const string EMAIL_PATTERN = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
+        #endregion const
+
+        #region attr
+        Regex regex = new Regex( EMAIL_PATTERN );
+        #endregion attr
+
+        #region private methods
+        void OnChanged(object sender, TextChangedEventArgs e)
+        {
+            var self = sender as Entry;
+            Console.WriteLine($"Entry.val: {e.NewTextValue}");
+            if (regex.IsMatch(e.NewTextValue)) self.TextColor = Color.Black;
+            else self.TextColor = Color.Red;
+        }
+        #endregion private methods
+
+        #region protected methods
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += OnChanged;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= OnChanged;
+        }
+        #endregion protected methods
+
+    }
+}
+
