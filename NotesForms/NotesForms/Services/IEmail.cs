f2742ff8a8c176e9e0a47f7fe1521f512ddd0869
Carlos May <carlosmg.may@icloud.com>
2023-05-25 11:31:13 -0400

    [UPDATE] Rename interface

---
+++ NotesForms/NotesForms/Services/IEmail.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NotesForms.Services
+{
+	public interface IEmail
+	{
+		void SendEmailMessage( string subject, string body, List<string> recipients);
+	}
+}
