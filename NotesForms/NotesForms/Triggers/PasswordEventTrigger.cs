7855b1c1d2bb94aa7c4f869fee04847d210900c5
Carlos May <carlosmg.may@icloud.com>
2023-05-26 17:29:56 -0400

    [CREATE] Triggers/PasswordEventTriggers

---
+++ NotesForms/NotesForms/Triggers/PasswordEventTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace NotesForms.Triggers
+{
+	public class PasswordEventTrigger:TriggerAction<Entry>
+	{
+        #region const
+        const int MIN_LENGTH = 6;
+        #endregion const
+
+        #region protected method
+        /// <summary>
+        /// Event called to validate de min lenght in an entry
+        /// </summary>
+        /// <param name="sender"></param>
+        protected override void Invoke(Entry sender)
+        {
+            if (sender.Text != null &&
+                 sender.Text.Length >= MIN_LENGTH)
+                sender.BackgroundColor = Color.Accent;
+            else
+                sender.BackgroundColor = Color.Transparent;
+        }
+        #endregion protected method
+       
+    }
+}
+
