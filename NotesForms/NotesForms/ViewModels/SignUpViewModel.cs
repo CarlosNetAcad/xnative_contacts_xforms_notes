ab83ed416a5ea33bd6551c00e5dd765d03b77550
Carlos May <carlosmg.may@icloud.com>
2023-05-26 19:12:07 -0400

    Create SignUpViewModel.cs

---
+++ NotesForms/NotesForms/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using ContactApp.Core.Entities;
+using ContactApp.Core.Repository;
+using NotesForms.ViewModels.Base;
+using Xamarin.Forms;
+
+namespace NotesForms.ViewModels
+{
+	public class SignUpViewModel:BaseVM
+	{
+        #region Attr
+        string _userName;
+
+        string _passWord;
+
+        string _fullName;
+
+        bool _canCreateAccount;
+
+        readonly INavigation _navigation;
+        #endregion Attr
+
+        #region Prop
+        public string UserName
+        {
+            get => _userName;
+            set => SetProperty( ref _userName,value );
+        }
+
+        public string PassWord
+        {
+            get => _passWord;
+            set => SetProperty( ref _passWord,value );
+        }
+
+        public string FullName
+        {
+            get => _fullName;
+            set => SetProperty( ref _fullName, value );
+        }
+
+        public bool CanCreateAccount
+        {
+            get => _canCreateAccount;
+            set => SetProperty( ref _canCreateAccount,value );
+        }
+        #endregion Prop
+
+        #region __constructors
+        public SignUpViewModel()
+		{
+		}
+        #endregion __constructors
+
+        #region methods
+
+        #endregion methods
+    }
+}
+
