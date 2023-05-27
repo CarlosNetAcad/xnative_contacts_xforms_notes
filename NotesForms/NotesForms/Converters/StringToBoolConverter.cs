84e09593075dd8cd3c0c9d364a516dc9ebb7fcdd
Carlos May <carlosmg.may@icloud.com>
2023-05-26 21:34:38 -0400

    [UPDATE] IMPLEMENT triggers to store user button

---
+++ NotesForms/NotesForms/Converters/StringToBoolConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace NotesForms.Converters
+{
+    public class StringToBoolConverter : IValueConverter
+    {
+        const int MIN_LENGTH = 6;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var text = value as String;
+
+            if (text != null &&
+                text.Length >= MIN_LENGTH)
+                return true;
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
