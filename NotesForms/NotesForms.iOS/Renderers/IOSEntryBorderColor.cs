c9c059b83b3fea3070445916f4d1328ea4cbb6bc
Carlos May <carlosmg.may@icloud.com>
2023-05-31 14:03:30 -0400

    [CREATE] Renderers when the control it is not build in forms

---
+++ NotesForms/NotesForms.iOS/Renderers/IOSEntryBorderColor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using NotesForms.iOS.Renderers;
+using NotesForms.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly:ExportRenderer( typeof(MyEntryBordered),typeof(IOSEntryBorderColor) )]
+namespace NotesForms.iOS.Renderers
+{
+	public class IOSEntryBorderColor:EntryRenderer
+	{
+        #region private method
+
+        void OnEditingChanged( object sender, EventArgs e)
+        {
+            var textField = sender as UITextField;
+
+            if ( textField.Text.Length >= 3 )
+            {
+                textField.Layer.BorderColor = UIColor.Red.CGColor;
+                textField.Layer.BorderWidth = 1;
+            }
+            else
+            {
+                textField.Layer.BorderWidth = 0;
+            }
+        }
+        #endregion private method
+
+        #region protected method
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if( e.OldElement != null )
+            {
+                Control.EditingChanged -= OnEditingChanged;
+            }
+
+            if( e.NewElement != null)
+            {
+                Control.EditingChanged += OnEditingChanged;
+            }
+        }
+        #endregion protected method
+    }
+}
+
---
+++ NotesForms/NotesForms.iOS/Renderers/IOSLongPressButton.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using NotesForms.iOS.Renderers;
+using NotesForms.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+namespace NotesForms.iOS.Renderers
+{
+	public class IOSLongPressButton:ButtonRenderer
+	{
+        #region attr
+        ICommand _command = null;
+        #endregion attr
+
+        #region private method
+        void OnLongPressed()
+        {
+            _command?.Execute(null);
+        }
+        #endregion private method
+
+        #region protected method
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                var longPressButton = Element as LongPressButton;
+                var longPressed = new UILongPressGestureRecognizer();
+                _command = longPressButton.LongPressCommand;
+
+                longPressed.AddTarget( OnLongPressed );
+
+                Control.AddGestureRecognizer( longPressed );
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if ( e.PropertyName == LongPressButton.LongPressCmdProp.PropertyName)
+            {
+                var longPressButton = Element as LongPressButton;
+
+                _command = longPressButton.LongPressCommand;
+            }
+            {
+
+            }
+        }
+        #endregion protected method
+    }
+}
