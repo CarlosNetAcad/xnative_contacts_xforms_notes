91f57778bef453898d972048b0e9444113a2ca40
Carlos May <carlosmg.may@icloud.com>
2023-06-09 16:13:01 -0400

    [UPDATE] Implement App:PrismApplication.

--- NotesForms/NotesForms/App.xaml.cs
+++ NotesForms/NotesForms/App.xaml.cs
@@ -6,125 +6,48 @@ using NotesForms.Pages;
 using NotesForms.Services;
 using NotesForms.Repository;
 using System.Diagnostics;
+using Prism;
+using Prism.Ioc;
+using Prism.DryIoc;
+
 
 namespace NotesForms
 {
-    public partial class App : Application, IPreferenceService
+    public partial class App : PrismApplication
     {
         #region Ctors
         /// <summary>
         /// This is the constructor
         /// ID string generated is "M:NotesForms.App.#ctor"
         /// </summary>
-        public App ()
+        public App()
+        { 
+        }
+
+        public App( IPlatformInitializer initializer,
+            bool setFormsDependencyResolver)
+            :base( initializer,setFormsDependencyResolver)
         {
-            InitializeComponent();
-
-            //-> Register services
-            DependencyService.RegisterSingleton<INoteService>( new SQLiteRepository() );
-            DependencyService.RegisterSingleton<IArticleService>( new APIRepository() );
-            DependencyService.RegisterSingleton<IAuthService>( new AuthService() );
-            DependencyService.RegisterSingleton<IUserService>( new UserService() );
-            DependencyService.RegisterSingleton<IGeolocation>( new GeoLocationService() );
-
-            MainPage = new NavigationPage( new SignInPage() );
         }
         #endregion Ctors
 
         #region - methods
-        /// <summary>
-        /// The <c>isLoggedIn</c> is ised to know the logged user state and the data persist on the app
-        /// ID string
-        /// </summary>
-        /// <param  name="isLoggedIn"></param>
-        /*void SetIsLogin(bool isLoggedIn)
-        {
-            if (this.Properties.ContainsKey("isLoggedIn"))
-            {
-                this.Properties["isLoggedIn"] = isLoggedIn;
-            }
-            else
-            {
-                this.Properties.Add("isLoggedIn", isLoggedIn);
-            }
-        }*/
         #endregion - methods
 
-        #region # methods
-        /// <summary>
-        /// App lifecycle, used when the app starts.
-        /// ID string generated is "M:NotesForms.App.OnStart"
-        /// </summary>
-        protected override void OnStart ()
-        {
-            var isLoggedIn = GetUsername();
-            if (String.IsNullOrEmpty(isLoggedIn)) SignOut();
-            else SignIn();
+        #region - events
+        #endregion - events
 
-        }
-
-        /// <summary>
-        /// App lifecycle, used when the app go to the background.
-        /// ID string generated is "M:NotesForms.App.OnSleep"
-        /// </summary>
-        protected override void OnSleep () {}
-
-        /// <summary>
-        /// App lifecycle, used when the app return from de background
-        /// ID string generated is "M:NotesForms.App.OnResume"
-        /// </summary>
-        protected override void OnResume() {}
+        #region # method
         #endregion # methods
 
-        #region + methods
-        
-        public void Set(string key, string value)
-        {
-            if (this.Properties.ContainsKey( key ))
-            {
-                this.Properties[key] = value;
-            }
-            else
-            {
-                this.Properties.Add( key, value );
-            }
-        }
+        #region # events
+        #endregion # events
 
-        public string Get(string key)
-        {
-            var existKey = Properties.TryGetValue( key, out object value );
-
-            if (existKey) return value as string;
-
-            return string.Empty;
-        }
-
-        public void UnSet(string key)
-        {
-            Properties.Remove( key );
-        }
-
-        public void SetUsername(string username)
-        {
-            Set("username", username);
-        }
-
-        public string GetUsername()
-        {
-            return Get("username");
-        }
-
-        public void SignIn()
-        {
-            MainPage = new MenuTabbedPage();
-        }
-
-        public void SignOut()
-        {
-            MainPage = new NavigationPage(new SignInPage());
-        }
-
+        #region + methods       
         #endregion + methods
+
+        #region + events       
+        #endregion + events
     }
 }
 
