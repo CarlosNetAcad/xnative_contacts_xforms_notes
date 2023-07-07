96ac11738176b9b4de80686a731508b3cf9d1dc1
Carlos May <carlosmg.may@icloud.com>
2023-07-07 11:57:37 -0400

    Create MVVMCross project.

---
+++ MvvmCross.Core/ViewModels/ContactDetailViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MvvmCross.ViewModels;
+using Microsoft.Extensions.Logging;
+using MvvmCross.Navigation;
+
+namespace Contacts.MvvmCross.Core.ViewModels
+{
+	public class ContactDetailViewModel: MvxNavigationViewModel
+    {
+		public ContactDetailViewModel(
+			ILoggerFactory logFactory,
+			IMvxNavigationService navigationService
+		) :base( logFactory, navigationService)
+		{
+		}
+	}
+}
+
---
+++ MvvmCross.Core/ViewModels/ContactsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
+
+namespace Contacts.MvvmCross.Core.ViewModels
+{
+    public class ContactsViewModel : MvxNavigationViewModel
+    {
+        public ContactsViewModel(
+            ILoggerFactory logFactory,
+            IMvxNavigationService navigationService) : base(logFactory, navigationService)
+        {
+        }
+    }
+}
+
