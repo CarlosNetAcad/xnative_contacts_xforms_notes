5059bc9b1771f3f9565c2c33fa40c26dffe5acef
Carlos May <carlosmg.may@icloud.com>
2023-07-07 11:56:49 -0400

    Create App.cs

---
+++ ContactApp.Core/MVVMCross/App.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MvvmCross;
+using MvvmCross.ViewModels;
+using System.Threading.Tasks;
+using MvvmCross.IoC;
+//using NotesCarlos.MvvmCross.Core.ViewModels;
+
+namespace ContactApp.Core.MVVMCross
+{
+	public class App
+	{
+		public App()
+		{
+		}
+	}
+}
+
